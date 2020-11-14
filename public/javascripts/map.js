7abd2778bd953271bdbf2b67651fe33a41385e73
nadpescador <santiago.calvi@davinci.edu.ar>
2020-11-13 22:28:27 -0300

    añadi el mapa con un archivo

---
+++ public/javascripts/map.js
@@ -0,0 +1,8 @@
+const map = L.map('main_map').setView([-34.6012424, -58.3861497],13);
+
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker ([-34.599564, -58.3778777]).addTo(map);
